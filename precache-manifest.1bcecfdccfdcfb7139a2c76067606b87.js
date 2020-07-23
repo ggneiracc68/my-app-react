9871f63667396722fb766b75ace887c39d8f6394
Gonzalo Neira Calsin <ggneiracc68@gmail.com>
2020-07-23 07:25:30 -0500

    Updates

---
+++ precache-manifest.1bcecfdccfdcfb7139a2c76067606b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3224d73c463c13088fd9a1d7eb5018ad",
+    "url": "/my-app-react/index.html"
+  },
+  {
+    "revision": "4e3d28311263d6616121",
+    "url": "/my-app-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app-react/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3d28311263d6616121",
+    "url": "/my-app-react/static/js/main.842c6dc8.chunk.js"
+  },
+  {
+    "revision": "a3e0b63c540b7240091c",
+    "url": "/my-app-react/static/js/runtime-main.8f1ffed2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
